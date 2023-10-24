218dccadf2eba64feff3f23f80d74e9e2d4d7ec6
hirofumi omi <omihirofumi@gmail.com>
2023-10-25 00:21:13 +0900

    update proxy.py

--- proxy.py
+++ proxy.py
@@ -22,3 +22,46 @@ def hexdump(src, length=16, show=True):
         for line in results:
             print(line)
 
+def receive_from(connection):
+    buffer = b""
+    connection.settimeout(5)
+    try:
+        while True:
+            data = connection.recv(4096)
+            if not data:
+                break
+            buffer += data
+    except:
+        pass
+
+    return buffer
+
+def request_handler(buffer):
+    return
+
+def response_handler(buffer):
+    return
+
+def proxy_handler(client_socket, remote_host, remote_port, recive_first):
+    remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    remote_socket.connect((remote_host, remote_port))
+
+    if recive_first:
+        remote_buffer = receive_from(remote_socket)
+        if len(remote_buffer):
+            print("[<==] Received %d bytes from remote." % len(remote_buffer))
+            hexdump(remote_buffer)
+
+            remote_buffer = response_handler(remote_buffer)
+            client_socket.send(remote_buffer)
+            print("[==>] Sent to local.")
+
+    while True:
+        local_buffer = receive_from(client_socket)
+        if len(local_buffer):
+            print("[<==] Received %d bytes from local." % len(local_buffer))
+            hexdump(local_buffer)
+            remote_socket.send(local_buffer)
+            print("[==>] Sent to remote")
+
+        remote_buffer = receive_from(remote_socket)
