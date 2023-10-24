67b2e928ed374d3a46258185711d8e9282d3849c
hirofumi omi <omihirofumi@gmail.com>
2023-10-25 00:30:07 +0900

    update proxy.py

--- proxy.py
+++ proxy.py
@@ -65,3 +65,16 @@ def proxy_handler(client_socket, remote_host, remote_port, recive_first):
             print("[==>] Sent to remote")
 
         remote_buffer = receive_from(remote_socket)
+        if len(remote_buffer):
+            print("[<==] Received %d bytes from remote." % len(remote_buffer))
+            hexdump(remote_buffer)
+
+            remote_buffer = response_handler(remote_buffer)
+            client_socket.send(remote_buffer)
+            print("[==>] Sent to local.")
+
+        if not len(local_buffer) or not len(remote_buffer):
+            client_socket.close()
+            remote_socket.close()
+            print("[*] No more data. Closing connections.")
+            break
