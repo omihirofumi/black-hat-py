cc061a344d400e1ab0dd3f5726a79aac0d48eeb9
hirofumi omi <omihirofumi@gmail.com>
2023-10-25 23:52:34 +0900

    complete proxt.py

--- proxy.py
+++ proxy.py
@@ -6,13 +6,14 @@ HEX_FILTER = ''.join(
     [(len(repr(chr(i))) == 3) and chr(i) or '.' for i in range(256)]
 )
 
+
 def hexdump(src, length=16, show=True):
     if isinstance(src, bytes):
         src = src.decode()
 
     results = list()
     for i in range(0, len(src), length):
-        word = str(src[i:i+length])
+        word = str(src[i:i + length])
 
         printable = word.translate(HEX_FILTER)
         hexa = ' '.join([f'{ord(c):02X}' for c in word])
@@ -21,6 +22,7 @@ def hexdump(src, length=16, show=True):
     if show:
         for line in results:
             print(line)
+
 
 def receive_from(connection):
     buffer = b""
@@ -36,11 +38,14 @@ def receive_from(connection):
 
     return buffer
 
+
 def request_handler(buffer):
     return
 
+
 def response_handler(buffer):
     return
+
 
 def proxy_handler(client_socket, remote_host, remote_port, recive_first):
     remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -78,3 +83,52 @@ def proxy_handler(client_socket, remote_host, remote_port, recive_first):
             remote_socket.close()
             print("[*] No more data. Closing connections.")
             break
+
+def server_loop(local_host, local_port, remote_host, remote_port, receive_first):
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        server.bind((local_host, local_port))
+    except Exception as e:
+        print('problem on bind: %r' % e)
+        print("[!!] Failed to listen on %s:%d" % (local_host, local_port))
+        print("[!!] Check for other listening sockets or correct permissions.")
+        sys.exit(0)
+
+    print("[*] Listening on %s:%d" % (local_host, local_port))
+    server.listen(5)
+    while True:
+        client_socket, addr = server.accept()
+        # 接続情報の出力
+        line = "> Received incoming connection from %s:%d" % (addr[0], addr[1])
+        print(line)
+        proxy_thread = threading.Thread(
+            target=proxy_handler,
+            args=(client_socket, remote_host, remote_port, receive_first),
+        )
+        proxy_thread.start()
+
+def main():
+    if len(sys.argv[1:]) != 5:
+        print("Usage: ./proxy.py [localhost] [localport]", end='')
+        print("[remotehost] [remoteport] [receive_first]")
+        print("Example: ./proxy.py 127.0.0.1 9000 10.12.132.1 9000 True")
+        sys.exit(0)
+
+    local_host = sys.argv[1]
+    local_port = int(sys.argv[2])
+
+    remote_host = sys.argv[3]
+    remote_port = sys.argv[4]
+
+    receive_first = sys.argv[5]
+
+    if "True" in receive_first:
+        receive_first = True
+    else:
+        receive_first = False
+
+    server_loop(local_host, local_port, remote_host, remote_port, receive_first)
+
+if __name__ == '__main__':
+    main()
+
